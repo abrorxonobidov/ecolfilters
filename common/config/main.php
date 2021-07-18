86a3c3ce2a808458f065f3bd5eb73149a5a99d30
abrorxonobidov <abrorkhon.obidov@gmail.com>
2021-07-19 00:15:11 +0500

    yii

---
+++ common/config/main.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'timeZone' => 'Asia/Tashkent',
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'i18n' => [
+            'translations' => [
+                'main*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'sourceLanguage' => 'uz',
+                    'fileMap' => [
+                        'main' => 'main.php',
+                        'main/error' => 'error.php'
+                    ]
+                ]
+            ]
+        ]
+    ],
+];
---
+++ frontend/config/main.php
@@ -0,0 +1,55 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'language' => 'uz',
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'ecofilters-frontend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'urlManager' => [
+            'class' => codemix\localeurls\UrlManager::class,
+            'enableDefaultLanguageUrlCode' => true,
+            'enableLanguagePersistence' => false,
+            'languages' => ['uz', 'ru'/*, 'en'*/],
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableLanguageDetection' => false,
+            'rules' => [
+                '' => '/site/index',
+                '<controller>/<action>/<id:\d+>' => '<controller>/<action>'
+            ]
+        ]
+    ],
+    'params' => $params,
+];
