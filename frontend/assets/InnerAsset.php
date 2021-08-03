91b49c3a8d663c6a084ed1f1cdd1c0cf0127bc03
abrorxonobidov <abrorkhon.obidov@gmail.com>
2021-08-03 23:36:31 +0500

    Asset fix

---
+++ frontend/assets/InnerAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class InnerAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/chosen.css',
+        'css/magiczoomplus.css',
+        'css/jquery-ui.css',
+        'css/style.css',
+        'css/media.css',
+        'css/style_my.css',
+    ];
+    public $js = [
+        'js/jquery-ui.js',
+        'js/main.js',
+        'js/my.js',
+        'js/magiczoomplus.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
