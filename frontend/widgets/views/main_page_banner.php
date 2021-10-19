a48e6b1232971341cc91f9dccd0f33eb3afe0ec0
abrorxonobidov <abrorkhon.obidov@gmail.com>
2021-10-19 23:38:56 +0500

    Main banner fix

--- frontend/widgets/views/main_page_banner.php
+++ frontend/widgets/views/main_page_banner.php
@@ -1,6 +1,6 @@
 <? /** @var $page common\models\Lists */ ?>
 <section class="block_2">
-    <span><img src="/img/section_2_image.jpg" alt=""/></span>
+    <span><img src="/uploads/<?=$page->preview_image?>" alt=""/></span>
     <div class="sec_2_text" style="background-color: rgba(255, 255, 255, 0.5); border-radius: 10px; padding: 20px;">
         <?= $page->previewLang ?>
     </div>
